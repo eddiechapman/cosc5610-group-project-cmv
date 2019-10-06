bc4c16853d70794d75e2e1a142b6533c371633ef
eddiechapman <eddie.chapman4@gmail.com>
2019-10-06 14:56:35 -0500

    IO Utils

--- group_project.py
+++ group_project.py
@@ -3,26 +3,29 @@ import pathlib
 
 
 PATH_SAMPLE_DATA = pathlib.Path('sample')
+PATH_ALL_TRAIN_DATA = pathlib.Path('data/all/train_period_data.jsonlist')
 
 
-def load_sample_data():
-    reddit_posts = []
-    for child in PATH_SAMPLE_DATA.iterdir():
+def read_json_dir(path=PATH_SAMPLE_DATA):
+    """
+    Read a directory of json files as a list of dictionaries.
+    """
+    reddit_data = []
+    for child in path.iterdir():
         with open(child.name, mode='r', encoding='utf-8') as f:
             post = json.loads(f)
-            reddit_posts.append(post)
+            reddit_data.append(post)
 
-    return reddit_posts
+    return reddit_data
 
 
-def main():
-    DATA_PATH = pathlib.Path('data/all/train_period_data.jsonlist')
-
-    with open(DATA_PATH, mode='r', encoding='utf-8') as f:
-        data = [json.loads(line) for line in f]
-
-    print(len(data))
+def read_jsonfile(path=PATH_ALL_TRAIN_DATA):
+    """
+    Read a jsonfile (one json per line) file as a list of dictionaries.
+    """
+    with open(path, mode='r', encoding='utf-8') as f:
+        return [json.loads(line) for line in f]
 
 
 if __name__ == '__main__':
-    main()
+    pass
