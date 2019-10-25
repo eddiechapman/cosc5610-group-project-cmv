4c7454465c16509c3be6ac4f613576224e0bcabd
Noah Asaria <noah.asaria@marquette.edu>
2019-10-25 18:27:10 -0500

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -2,13 +2,29 @@ import csv
 import datetime
 import json
 import logging
-import pathlib
+import pathlib 
 import pprint
+import parse_data as pd
 
-#from models import Submission, Comment, User, BOTLIST, DATE_START, DATE_END
+from models import Submission, Comment, User, BOTLIST, DATE_START, DATE_END
 
+#TO DO: Go through list, apply function or check value --> If return true.
 # filterSubmissionsByTime: list of submission objects, startTime (datetime), endTime (datetime) --> list of submission objects
-def filterSubmissionsByTime(submissionList, startTime, endTime):
-    for submission in submissionList:
+def filterSubmissionsByTime(JSONList):
+    submissions = []
+    for i in range(len(JSONList)): 
+        submissions.append(pd.create_submission(JSONList[i]))
+    
+    for i in range(len(submissions)):
+        print(submissions[i].time)
+
+
+def grabTimeFromJSON(JSONList):
+    for i in range(len(JSONList)):
+        print(JSONList[i].keys())
 
 # filterJSONListByTime: jsonlist, startime (datetime), endtime (datetime) --> jsonlist
+
+
+data = pd.load_examples()
+grabTimeFromJSON(data)
