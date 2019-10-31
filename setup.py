f4cb7294561a5a75ef830b9df04bafd68da6fce9
eddiechapman <eddie.chapman4@gmail.com>
2019-10-31 14:54:56 -0500

    Long description from README

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='cosc5610-group-project-cmv',
     version='0.1',
     packages=['cmv'],
     install_requires=[],  # TODO: How to autoload from requirements.txt?
     description='Network analysis of the ChangeMyView dataset',
+    long_description=readme(),
     author='Noah Asaria, David Reddy, Eddie Chapman',
     url='https://github.com/eddiechapman/cosc5610-group-project-cmv',
 )
