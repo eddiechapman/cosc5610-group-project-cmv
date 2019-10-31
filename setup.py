851beccc5fdf6d2321b52707ed095f0581047a0d
eddiechapman <eddie.chapman4@gmail.com>
2019-10-31 14:52:05 -0500

    Need to autoload requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name='cosc5610-group-project-cmv',
     version='0.1',
     packages=['cmv'],
+    install_requires=[],  # TODO: How to autoload from requirements.txt?
     description='Network analysis of the ChangeMyView dataset',
     author='Noah Asaria, David Reddy, Eddie Chapman',
     url='https://github.com/eddiechapman/cosc5610-group-project-cmv',
