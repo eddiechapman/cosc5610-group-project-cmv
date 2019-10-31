524b46af1805f7498474fdc3f328b38e384364b6
eddiechapman <eddie.chapman4@gmail.com>
2019-10-31 14:23:04 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils import setup
+
+setup(
+    name='cosc5610-group-project-cmv',
+    version='1.0',
+    description='Network analysis of the ChangeMyView dataset',
+    author='Noah Asaria, David Reddy, Eddie Chapman',
+    url='https://github.com/eddiechapman/cosc5610-group-project-cmv',
+    packages=['change_my_view'],
+    scripts=['scripts']
+)
+
