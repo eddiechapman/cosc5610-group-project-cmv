a6ebd19ab86d9778165cb9c85366ffb907da07c9
eddiechapman <eddie.chapman4@gmail.com>
2019-10-31 14:46:35 -0500

    Use setuptools.

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from distutils import setup
-
+from setuptools import setup
 setup(
     name='cosc5610-group-project-cmv',
-    version='1.0',
+    version='0.1',
+    packages=['cmv'],
     description='Network analysis of the ChangeMyView dataset',
     author='Noah Asaria, David Reddy, Eddie Chapman',
     url='https://github.com/eddiechapman/cosc5610-group-project-cmv',
-    packages=['change_my_view'],
-    scripts=['scripts']
 )
 
