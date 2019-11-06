6d6a4dbc18de4bcc6e1d6a54655fd885cf393ba3
Noah Asaria <noah.asaria@marquette.edu>
2019-11-06 17:41:28 -0600

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,9 @@
+#At point of "freezing" (i.e. a reputable poster has entered the scence), measure:
+    #1) Rate of co-commenting: When a 200 delta power user has entered, do we see a higher 
+        # of replies/(min/sec/...) than before? Hypothesis: A reputable user entering increases the activity 
+    #2) Rate of delta awarding: Aggregated, what is the rate at which deltas are awarded in the thread
+        # before the first reputable user, second user, ... 
+        # Hypothesis: Reputable users make more convincing arguments, and attract other high-quality users.
+            #Therefore, there will be a spike in deltas being awarded once reputable users arrive and the OP will be more willing to consider arguments as ewll
+    #3) Topic modeling: How similar are reputable users in shared interests to OP/co-commentators. What about the users that followed
+        #I.e. do reputable users attract similarly minded users?
